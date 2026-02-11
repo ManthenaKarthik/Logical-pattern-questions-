3d5808c424006d06514cc49f26c018ccdf6a18f4
ManthenaKarthik <manthenakarthik604@gmail.com>
2026-02-11 21:08:20 +0530

    Add files via upload

---
+++ diagonal.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+ int row,col;
+ printf("enter values of row and column\n");
+ scanf("%d%d",&row,&col);
+ for(int i=1;i<=row;i++)
+ {
+  for(int j=1;j<=col;j++)
+   {
+    if (i==1||i==row||j==1||j==col||i==j)
+     {
+     printf("*");
+     }
+     else
+     {
+      printf(" ");
+     }
+    }
+   printf("\n");
+ }
+ 
+}
